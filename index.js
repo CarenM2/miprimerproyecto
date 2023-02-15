d2e530cb70030e7bbbd4b8e11960da13b266c65f
Carretero Caren <Carretero0896@gmail.com>
2023-02-15 15:57:16 -0300

    primer escritura en el repositorio

---
+++ index.js
@@ -0,0 +1,13 @@
+function initMap() {
+ const posicion = {
+
+    lat: -25.363,
+    lng: 131.044
+ }
+
+const map = new google.maps.Map(document.getElementById("map"), {
+
+    zoom: 4,
+    center: posicion
+})
+}
